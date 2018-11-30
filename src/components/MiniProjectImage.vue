30d0b0492c1be3e51f96cac063b77c027d2ea7df
zach <zaspringer@gmail.com>
2018-11-30 16:01:56 -0700

    Giving up on the transition for the moment. No idea why its not working but I will come back to adding it in if dad wants it on the site

--- src/components/MiniProjectImage.vue
+++ src/components/MiniProjectImage.vue
@@ -1,23 +1,7 @@
 <template>
-    <transition name="mini-image"
-    v-on:before-enter="enter"
-    v-on:enter="afterEnter"
-    v-on:leave="leave">
-        <div v-if="selected">
-            <img class="thumbnail-image" :src="link"/>
-        </div>
-        <div v-else>
-            <img class="thumbnail-image" :src="link"/>
-        </div>
-    </transition>
-    <!-- <transition name="mini-image">
-        <div v-if="selected" class="thumbnail-image-container active-mini-image">
-            <img class="thumbnail-image" :src="link"/>
-        </div>
-        <div v-else class="thumbnail-image-container">
-            <img class="thumbnail-image" :src="link"/>
-        </div>
-    </transition> -->
+    <div v-bind:class="[selected ? active : '', thumbnailImageContainer]">
+        <img class="thumbnail-image" :src="link"/>
+    </div>
 </template>
 
 <script>
@@ -29,47 +13,23 @@ export default {
             thumbnailImageContainer: 'thumbnail-image-container',
             active: 'active-mini-image'
         }
-    },
-    methods: {
-        enter(el) {
-            console.log(1)
-        },
-        afterEnter(el) {
-            console.log(2)
-
-        },
-        leave(el) {
-            console.log(3)
-
-        },
-        afterLeave(el) {
-            console.log(4)
-        }
     }
 }
 </script>
 
 <style>
-/* .thumbnail-image-container {
+.thumbnail-image-container {
     width: 100%;
     height: 100px;
     margin-bottom: 10px;
+    
 }
 .active-mini-image {
-    height: 120px;
-    transition: height 2s;
-} */
-.mini-image-enter {
-    width: 100%;
-    height: 100px;
-    margin-bottom: 10px;
-}
-.mini-image-leave {
-    height: 120px;
-    transition: height 2s;
+
 }
 .thumbnail-image {
     width: 100%;
     height: 100%;
+    transition: height 2s;
 }
 </style>
